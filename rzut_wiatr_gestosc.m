955c434ec6405f9424fc122e76c3f547a6562457
BartlomiejKoziel <hokijon10@gmail.com>
2024-01-21 12:29:57 +0100

    Update rzut_wiatr_gestosc.m
    poprawiona gestosc, dodany wykres

--- rzut_wiatr_gestosc.m
+++ rzut_wiatr_gestosc.m
@@ -9,7 +9,7 @@ C = 0.25;    % Drag Coefficient of a Sphere
 
 % Initial Conditions
 delta_t = 0.001; % s
-h = 0.0001;         % m (initial height) czemu tutaj nie może być 0???
+h = 5;         % m (initial height) czemu tutaj nie może być 0???
 
 % Case 1: Free-Falling Bullet
 x(1) = 0;       % m
@@ -24,26 +24,36 @@ t(1) = 0;
 i = 1;          % Sets Counter/Index
 
 % Wind parameters
-wind_speed = 0;    % m/s
+wind_speed = 10;    % m/s
 wind_angle = 0;    % degree
 
 % Start Loop For Projectile Motion with Air Resistance and Wind
 while (y(i) > 0)
     % Calculate gęstość powietrza using the formula mentioned earlier
-    % TRZEBA PRZELICZYC WARTOSCI BO COS CHUJOWE SA
-    rho = 1.225 * (1 - 0.0065 * h / 288.15)^(5.2561);
+    if h < 11000
+        L = 0.0065; % Example value for variable L, you can replace it with your specific variation
+        T = 288.5;
+    elseif h <= 20000 && h >= 11000
+        L = 0;
+        T=216.65;
+    elseif h>20000
+        L = -0.001;
+        T = 288.15;
+    end
+    
+    rho = 1.225 * (1 - (L * h) / T)^((0.0341628 / L) - 1);
 
     % Drag calculation
     A = (pi * (r * 2)^2) / 4;
     b = (rho * C * A) / (2 * m);
-    ax = -(b / m) * v * vx;
+    ax = -(b / m) * vx^2;
 
     % Wind effect
-    ax_wind = -(wind_speed * cosd(wind_angle));
+    ax_wind = (wind_speed * cosd(wind_angle));
     ax = ax + ax_wind;
 
     % Vertical motion
-    ay = -9.81 - (b / m) * v * vy;
+    ay = -9.81 - (b / m) * vy^2;
     v = sqrt(vx^2 + vy^2);
     vx = vx + ax * delta_t;
     vy = vy + ay * delta_t;
@@ -56,7 +66,8 @@ while (y(i) > 0)
     % Store acceleration and velocity data for plotting
     acceleration(i) = ax;
     velocity_x(i) = vx;
-
+    velocity_y(i) = vy;
+    velocity(i)=v;
     % Update height for the next iteration
     h = h - vy * delta_t;
 
@@ -80,7 +91,15 @@ title('Changes in Horizontal Acceleration');
 
 % Plot Changes in Horizontal Velocity
 subplot(3, 1, 3);
-plot(t(1:i-1), velocity_x, 'g');
+plot(t(1:i-1), velocity, 'g');
 xlabel('Time (s)');
 ylabel('Vx (m/s)');
 title('Changes in Horizontal Velocity');
+
+figure;
+plot(x(1:i-1), y(1:i-1), 'k');
+xlabel('Horizontal Distance (m)');
+ylabel('Vertical Distance (m)');
+title('Vertical Position vs Horizontal Position');
+axis equal;
+
